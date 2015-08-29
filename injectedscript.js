402c24f98ae07be15c2489a286319038516ca537
vanuan <vanuan@gmail.com>
2015-08-30 00:53:15 +0300

    Another bug

--- injectedscript.js
+++ injectedscript.js
@@ -4,16 +4,18 @@
   window.injectedScript = function injectedScript() {
     var listContainerSelector = "section:not('.fltr') > div.reducer > .list";
     // sorting
-    $(listContainerSelector + " > .list_row").sort(function(a, b) {
-      function getVotes(el) {
-        return parseInt($(el).find('span').text())
-      };
-      if (getVotes(a) > getVotes(b)) {
-        return -1;
-      } else {
-        return 1;
-      }
-    }).appendTo(listContainerSelector);
+    var sort = function sort() {
+      $(listContainerSelector + " > .list_row").sort(function(a, b) {
+        function getVotes(el) {
+          return parseInt($(el).find('span').text())
+        };
+        if (getVotes(a) > getVotes(b)) {
+          return -1;
+        } else {
+          return 1;
+        }
+      }).appendTo(listContainerSelector);
+    };
     // filtering
     $(".txt_input").bind('input', function(ev) {
       $(listContainerSelector + " > .list_row").css('display', 'table-row');
@@ -22,7 +24,9 @@
           return !($(el).find('a').text().toLowerCase()
                    .includes(ev.target.value.toLowerCase()));
         }).css("display", "none");
-      }
+      };
+      sort();
     });
+    sort();
   }
 })();
