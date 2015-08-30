49d4cba6be5286165010352724b58f5a9e87d9ea
vanuan <vanuan@gmail.com>
2015-08-30 21:17:58 +0300

    Update structure

--- extension/petition-sorted.user.js
+++ extension/petition-sorted.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name           Сортуй та фільтруй петиції!
 // @namespace      vanuan
-// @require        http://vanuan.github.io/petition-sorted/injectedscript.js
-// @require        http://vanuan.github.io/petition-sorted/contentscript.js
+// @require        http://vanuan.github.io/petition-sorted/extension/injectedscript.js
+// @require        http://vanuan.github.io/petition-sorted/extension/contentscript.js
 // @include        https://petition.president.gov.ua/petition/archive
 // @include        https://petition.president.gov.ua/?status=*
 // @include        https://petition.president.gov.ua/search?text=*
