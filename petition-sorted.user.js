d4fd344dd796bd02d46857f639557acb92c1bf0d
vanuan <vanuan@gmail.com>
2015-08-30 18:51:36 +0300

    Search page sorting

--- petition-sorted.user.js
+++ petition-sorted.user.js
@@ -5,5 +5,6 @@
 // @require        http://vanuan.github.io/petition-sorted/contentscript.js
 // @include        https://petition.president.gov.ua/petition/archive
 // @include        https://petition.president.gov.ua/?status=*
+// @include        https://petition.president.gov.ua/search?text=*
 // @include        https://petition.president.gov.ua/
 // ==/UserScript==
