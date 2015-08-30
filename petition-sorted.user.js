42737bc6ab81a8dd8d42137641ea100f79719210
vanuan <vanuan@gmail.com>
2015-08-30 18:39:05 +0300

    Add user script

---
+++ petition-sorted.user.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name           Сортуй та фільтруй петиції!
+// @namespace      vanuan
+// @require        http://vanuan.github.io/petition-sorted/injectedscript.js
+// @require        http://vanuan.github.io/petition-sorted/contentscript.js
+// @include        https://petition.president.gov.ua/petition/archive
+// @include        https://petition.president.gov.ua/?status=*
+// @include        https://petition.president.gov.ua/
+// ==/UserScript==
